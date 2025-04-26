60388ede4de98ec936ccda2120a71a558ef23399
Pedro Ramos <pedrogabrielguimaraesramos178@gmail.com>
2025-04-26 00:13:38 -0300

    Primeiro commit

---
+++ aula4/aula4.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title> Aula 4 PHP</title>
+    <link rel="stylesheet" href="estilo.css"/>
+
+</head>
+<body>
+<div>
+    <?php
+            /*$n = 4;
+            //echo $n;
+            //echo "<br>";
+            $nome = "Pedro";
+            $n = 4.5;
+            echo $no;
+            echo "<br>";
+            echo $n;
+            echo "<br>";*/
+
+            /*$nome = "Pedro";
+            $idade = 19;
+            echo"$nome"." tem ".$idade." anos!";
+            echo "<br>";
+            echo "$nome tem $idade anos!";*/
+
+    ?>
+</div>
+</body>
+</html>
---
+++ olamundo/ola.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title> Primeiro Exemplo PHP</title>
+    <style>
+        h2{
+            color:purple;
+            text-shadow: 1px 1px 1px black;
+        }
+    </style>
+</head>
+<body>
+    <h1>Testando PHP</h1>
+    <?php
+    echo "<h2><u>Olá <br>Mundo!</u></h2>";
+
+    ?>
+
+</body>
+</html>
